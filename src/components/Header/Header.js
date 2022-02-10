b43e5ece8f7967b0825552f474ce41635eccd8a6
Jaeho <benteke@naver.com>
2022-02-10 23:12:59 +0900

    Add:레이아웃 중간완료

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,34 @@
+import * as S from './Header_Style';
+import { AiFillStar } from 'react-icons/ai';
+import { BiChevronDown } from 'react-icons/bi';
+import { BsFillChatFill } from 'react-icons/bs';
+import { BiDockTop } from 'react-icons/bi';
+import { FaUserAlt } from 'react-icons/fa';
+
 export default function Header() {
-  return <div>오 된다된다 </div>;
+  return (
+    <S.HeaderWrap>
+      <S.Header>
+        <S.LeftHeader>
+          <AiFillStar />
+          <S.HeaderText>Genernal</S.HeaderText>
+          <BiChevronDown />
+        </S.LeftHeader>
+        <S.CenterHeader>
+          <S.ChannelsBox>
+            <BsFillChatFill />
+            <S.Channels>Channels</S.Channels>
+          </S.ChannelsBox>
+          <S.IdeationBox>
+            <BiDockTop />
+            <S.Ideation>Ideation</S.Ideation>
+          </S.IdeationBox>
+        </S.CenterHeader>
+        <S.RightHeader>
+          <FaUserAlt />
+          <S.UserCount>5</S.UserCount>
+        </S.RightHeader>
+      </S.Header>
+    </S.HeaderWrap>
+  );
 }
---
+++ src/components/Header/Header_Style.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const HeaderWrap = styled.div``;
+
+export const Header = styled.div`
+  border: 1px solid;
+`;
+
+export const LeftHeader = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const HeaderText = styled.div`
+  padding: 0px 5px 0px 5px;
+`;
+export const CenterHeader = styled.div``;
+
+export const ChannelsBox = styled.div``;
+
+export const Channels = styled.div``;
+
+export const IdeationBox = styled.div``;
+
+export const Ideation = styled.div``;
+
+export const RightHeader = styled.div``;
+
+export const UserCount = styled.div``;
