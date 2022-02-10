3a9533afe721e198a87e9d024fdbe551ee905f0b
JooyoungHwnag <joodudsdx@gmail.com>
2022-02-11 01:11:29 +0900

    Add: 로그인 레이아웃 완료

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,23 +1,30 @@
 import * as S from './Login_Style';
-import { HiOutlineCamera } from 'react-icons/hi';
 
 export default function Login() {
   return (
-    <S.Login>
-      <S.LoginWrap>
-        <S.LoginTitle>Swit 회원가입</S.LoginTitle>
-        <S.ProfileContainer>
-          <S.RoundProfile>
-            <S.ProfilePreviewImage src="" />
-          </S.RoundProfile>
-          <S.ProfileUpload />
-        </S.ProfileContainer>
-        <S.LoginForm>
-          <S.UserIdInput />
-          <S.UserNicknameInput />
-          <S.FormSubmit>Start Now</S.FormSubmit>
-        </S.LoginForm>
-      </S.LoginWrap>
-    </S.Login>
+    <>
+      <S.Logo />
+      <S.Login>
+        <S.LoginWrap>
+          <S.LoginTitle>Swit 회원가입</S.LoginTitle>
+          <S.ProfileContainer>
+            <S.RoundProfile>
+              <S.ProfilePreviewImage src="" />
+            </S.RoundProfile>
+            <S.UploadBtn>
+              <S.Test />
+            </S.UploadBtn>
+            <S.ProfileUpload />
+          </S.ProfileContainer>
+          <S.LoginForm>
+            <S.Label>아이디</S.Label>
+            <S.UserIdInput />
+            <S.Label>닉네임</S.Label>
+            <S.UserNicknameInput />
+            <S.FormSubmit>Start Now</S.FormSubmit>
+          </S.LoginForm>
+        </S.LoginWrap>
+      </S.Login>
+    </>
   );
 }
--- src/pages/login/Login_Style.js
+++ src/pages/login/Login_Style.js
@@ -1,10 +1,18 @@
 import styled from 'styled-components';
 import ProfileDefaultImg from '../../assets/icons/profile-default.jpeg';
+import { HiOutlineCamera } from 'react-icons/hi';
+import LogoImage from '../../assets/logo-big.svg';
+
+export const Logo = styled.img.attrs({
+  src: LogoImage,
+})`
+  margin: 20px;
+`;
 
 export const Login = styled.section`
-  width: 800px;
+  width: 400px;
   margin: 0 auto;
-  background: #eeeeee;
+  background: #f8f8f8;
   padding: 70px;
 `;
 
@@ -14,7 +22,6 @@ export const LoginWrap = styled.div`
   align-items: center;
   justify-content: center;
   padding: 40px;
-  background: #e33;
 `;
 
 export const LoginTitle = styled.h1`
@@ -22,11 +29,17 @@ export const LoginTitle = styled.h1`
   font-weight: bold;
 `;
 
-export const ProfileContainer = styled.div``;
+export const ProfileContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 40px;
+`;
 
 export const RoundProfile = styled.div`
-  width: 50px;
-  height: 50px;
+  width: 60px;
+  height: 60px;
   border-radius: 50%;
   overflow: hidden;
 `;
@@ -37,28 +50,80 @@ export const ProfilePreviewImage = styled.img.attrs(props => ({
   width: 100%;
 `;
 
+export const UploadBtn = styled.label.attrs({
+  for: 'fileInput',
+})`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: #fff;
+  overflow: hidden;
+  z-index: 1;
+  cursor: pointer;
+`;
+
+export const Test = styled(HiOutlineCamera)`
+  position: absolute;
+  top: 2px;
+  left: 50%;
+  transform: translateX(-50%);
+`;
+
 export const ProfileUpload = styled.input.attrs({
+  id: 'fileInput',
   type: 'file',
-  size: '1em',
-})``;
+})`
+  position: absolute;
+  bottom: 0;
+  display: none;
+`;
 
 export const LoginForm = styled.form`
   display: flex;
+  width: 300px;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  margin-top: 30px;
+`;
+
+export const Label = styled.label`
+  font-size: 10px;
+  color: #2b2b2b;
+  align-self: flex-start;
+  margin: 20px 0 5px 0;
 `;
 
 export const UserIdInput = styled.input.attrs({
   type: 'text',
   placeholder: 'ID를 입력해주세요.',
-})``;
+})`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ebebeb;
+  border-radius: 5px;
+`;
 
 export const UserNicknameInput = styled.input.attrs({
   type: 'text',
   placeholder: '닉네임을 입력해주세요.',
-})``;
+})`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ebebeb;
+  border-radius: 5px;
+`;
 
 export const FormSubmit = styled.button.attrs({
   type: 'button',
-})``;
+})`
+  margin-top: 40px;
+  padding: 10px 30px;
+  border: none;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #478bff;
+`;
