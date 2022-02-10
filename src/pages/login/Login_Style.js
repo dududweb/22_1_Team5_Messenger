2afdb3e74b653dbbb6b87f8230e59e4988cb9a16
JooyoungHwnag <joodudsdx@gmail.com>
2022-02-11 01:27:07 +0900

    Fix: 로그인wrap padding 값 수정

--- src/pages/login/Login_Style.js
+++ src/pages/login/Login_Style.js
@@ -21,7 +21,7 @@ export const LoginWrap = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  padding: 40px;
+  padding: 20px;
 `;
 
 export const LoginTitle = styled.h1`
