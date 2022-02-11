01d27a7b1b7a6df8dc7b58300e4cca1f4fa66227
Moro-yong <g76eho@gmail.com>
2022-02-11 15:03:17 +0900

    Add: 로그인 redux 추가

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
+import Aaa from './components/Aaaa';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Aaa />} />
       </Routes>
     </BrowserRouter>
   );
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
 import { Reset } from 'styled-reset';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Reset />
-    <Router />
+    <Provider store={store}>
+      <Router />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
