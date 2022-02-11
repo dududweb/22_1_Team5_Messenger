26ea22c9a10d57c58f1d731be64de0e9523265e3
Jaeho <benteke@naver.com>
2022-02-11 21:14:44 +0900

    Add: Redux DevTool 추가

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 import Router from './Router';
 import { Reset } from 'styled-reset';
 import { Provider } from 'react-redux';
-import { store } from './redux/store';
+import { createStore } from 'redux';
+import rootReducer from './redux/reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
